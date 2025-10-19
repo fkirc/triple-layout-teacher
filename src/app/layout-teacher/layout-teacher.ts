a69492f57f1ea3b419277cc21b97ff3d7f7d8a0d
nomo-fe <dev2@nomo.app>
2025-10-19 23:50:05 +0200

    feat: getParentStyle

--- src/app/layout-teacher/layout-teacher.ts
+++ src/app/layout-teacher/layout-teacher.ts
@@ -11,6 +11,23 @@ const layoutAlgorithms = [
 ] as const;
 type LayoutAlgorithm = typeof layoutAlgorithms[number];
 
+function getParentStyle(algorithm: LayoutAlgorithm): string {
+  switch (algorithm) {
+    case 'block':
+      return 'display: block;';
+    case 'flex':
+      return 'display: flex;';
+    case 'grid':
+      return 'display: grid; grid-template-columns: repeat(2, 1fr);';
+    case 'absolute':
+      return 'position: relative;';
+    case 'float':
+      return 'float: left; width: 100%;';
+    default:
+      return '';
+  }
+}
+
 @Component({
   selector: 'app-layout-teacher',
   imports: [CommonModule, FormsModule],
@@ -22,12 +39,14 @@ export class LayoutTeacher implements OnInit {
   algorithms = layoutAlgorithms
 
   selectedAlgorithm: LayoutAlgorithm = 'block';
+  playgroundParentStyle: string = '';
 
   ngOnInit(): void {
     this.selectedAlgorithm = layoutAlgorithms[0];
+    this.playgroundParentStyle = getParentStyle(this.selectedAlgorithm);
   }
 
   onChange(): void {
-    console.log('New algorithm selected:', this.selectedAlgorithm);
+    this.playgroundParentStyle = getParentStyle(this.selectedAlgorithm);
   }
 }
