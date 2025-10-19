ea0f6708da5abb6a281780c85253b61bce3417f2
fk <17876666+fkirc@users.noreply.github.com>
2025-10-19 21:33:47 +0000

    feat: selectedAlgorithm

--- src/app/layout-teacher/layout-teacher.ts
+++ src/app/layout-teacher/layout-teacher.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+const layoutAlgorithms = [
+  "block",
+  "flex",
+  "grid",
+  "absolute",
+  "float"
+] as const;
+type LayoutAlgorithm = typeof layoutAlgorithms[number];
 
 @Component({
   selector: 'app-layout-teacher',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './layout-teacher.html',
   styleUrl: './layout-teacher.css'
 })
-export class LayoutTeacher {
+export class LayoutTeacher implements OnInit {
 
+  algorithms = layoutAlgorithms
+
+  selectedAlgorithm: LayoutAlgorithm = 'block';
+
+  ngOnInit(): void {
+    this.selectedAlgorithm = layoutAlgorithms[0];
+  }
+
+  onChange(): void {
+    console.log('New algorithm selected:', this.selectedAlgorithm);
+  }
 }
